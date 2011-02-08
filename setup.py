cba0f4e70b6747311222cd6a68d07de5b4cac95e
Thom Linton <thom.linton@gmail.com>
2011-02-07 17:34:38 -0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "django-downtime",
+    version = "0.1.0",
+    author = "Thom Linton",
+    author_email = "thom.linton@gmail.com",
+    description = "A painfully simple Django application to statically generate a maintenance landing page."
+    license = "BSD",
+    keywords = "",
+    url = "http://github.com/yorkedork/django-downtime/tree/master",
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+    ],
+    package_data={},
+)
